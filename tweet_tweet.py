91e52ecf09f5f1f8bd00c75af8be31aa0f677d81
superkoteman <superkoteman@mail.ru>
2022-02-09 16:39:31 +0300

    adding script

---
+++ tweet_tweet.py
@@ -0,0 +1,28 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler(input("Input your consumer key, please: "), input("Input your consumer secret, please: "))
+auth.set_access_token(input("Input your access token, please: "), input("Input your access token secret, please: "))
+
+api = tweepy.API(auth)
+user = api.verify_credentials()
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.TooManyRequests:
+        time.sleep(300)
+    except StopIteration:
+        return
+
+search_string = input("Input key text for searching, please: ")
+numbers_of_tweets = int(input("Input number of tweets you want to like, please: "))
+
+for tweet in tweepy.Cursor(api.search_tweets, search_string).items(numbers_of_tweets):
+    try:
+        tweet.favorite()
+    except tweepy.TweepyException as e:
+        print(e.reason)
+    except StopIteration:
+        break
